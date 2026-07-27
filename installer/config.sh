f15cca1bace228cc0063962988161ffb860717bf
carzyriff <riturajsaha19hi@gmail.com>
2026-07-27 21:44:16 +0530

    updated script

--- installer/config.sh
+++ installer/config.sh
@@ -2,6 +2,9 @@
 
 echo "Backing up configs..."
 
+set -e
+
+dotfiles="$HOME/dotfiles"
 backup="$HOME/.config-backup-$(date +%Y%m%d-%H%M%S)"
 conf="$HOME/.config"
 
@@ -29,7 +32,12 @@ done
 echo "Installing configs..."
 
 for cfg in "${configs[@]}"; do
-  cp -r "$cfg" "$conf"
+  if [[ -d "$dotfiles/$cfg" ]]; then
+    echo "Installing $cfg..."
+    cp -r "$dotfiles/$cfg" "$conf/"
+  else
+    echo "Warning: '$dotfiles/$cfg' not found! Skipping..."
+  fi
 done
 
 echo "Done!"
