005578b16a8b881e738449db7fdb54014643da5b
riff <riturajsaha19hi@gmail.com>
2026-07-28 23:31:47 +0530

    23:31,28-07-2026

--- hypr/scripts/hyprlock.sh
+++ hypr/scripts/hyprlock.sh
@@ -1,2 +1,14 @@
 #!/bin/zsh
-hyprlock
+
+WALLPAPER=$(readlink -f ~/.config/hypr/current_wallpaper)
+
+if [[ "$WALLPAPER" == *.gif ]]; then
+    TMP_WALL=/tmp/hyprlock-frame.png
+    ffmpeg -y -i "$WALLPAPER" -vframes 1 "$TMP_WALL" 2>/dev/null
+    ORIG_TARGET=$(readlink ~/.config/hypr/current_wallpaper)
+    ln -sf "$TMP_WALL" ~/.config/hypr/current_wallpaper
+    hyprlock
+    ln -sf "$ORIG_TARGET" ~/.config/hypr/current_wallpaper
+else
+    hyprlock
+fi
